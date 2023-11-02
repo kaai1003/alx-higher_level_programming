7fd5bc1f55aca12a7e0df1bbe1b7370793d02765
aimad kacem <kacem.aimad@gmail.com>
2023-11-02 08:17:01 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
-print("{0:d} + {1:d} = {2:d}".format(a, b, add_0.add(a, b)))
+print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
