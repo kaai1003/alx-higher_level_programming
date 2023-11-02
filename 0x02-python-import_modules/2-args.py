0ee8cba10b4f451558edfcc3384d29cac1ced946
aimad kacem <kacem.aimad@gmail.com>
2023-11-02 15:17:36 -0700

    modify code args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,12 @@
 if __name__ == "__main__":
     import sys
 
-n = len(sys.argv)
-if n == 1:
-    print("{0:d} arguments.".format(n - 1))
-elif n == 2:
-    print("{0:d} argument :".format(n - 1))
-    for ar in range(1, n):
-        print("{0:d}: {1}".format(ar, sys.argv[ar]))
+n = len(sys.argv) - 1
+if n == 0:
+    print("0 arguments.")
+elif n == 1:
+    print("1 argument :")
 else:
-    print("{0:d} arguments.".format(n - 1))
-    for ar in range(1, n):
-        print("{0:d}: {1}".format(ar, sys.argv[ar]))
+    print("{0:d} arguments.".format(n))
+for ar in range(1, (n + 1)):
+    print("{0:d}: {1}".format(ar, sys.argv[ar]))
