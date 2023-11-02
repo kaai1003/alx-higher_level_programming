9c0a539810b8d73f49da005840c7607555e8d192
aimad kacem <kacem.aimad@gmail.com>
2023-11-02 15:19:58 -0700

    modify code args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ n = len(sys.argv) - 1
 if n == 0:
     print("0 arguments.")
 elif n == 1:
-    print("1 argument :")
+    print("1 argument:")
 else:
-    print("{0:d} arguments.".format(n))
+    print("{0:d} arguments:".format(n))
 for ar in range(1, (n + 1)):
     print("{0:d}: {1}".format(ar, sys.argv[ar]))
