80637ed154f694715600c3c66be69e04f22ba5a7
aimad kacem <kacem.aimad@gmail.com>
2023-11-02 15:59:44 -0700

    5-variable_load.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import  hidden_4
+    import hidden_4
 
 noms = dir(hidden_4)
 for nom in noms:
     if nom[0:2] != "__":
-        print(f"{nom}")
+        print("{0}".format(name))
