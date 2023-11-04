bc42b3e837fe66c2807fc6984042044e596025da
aimad kacem <kacem.aimad@gmail.com>
2023-11-04 14:46:10 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     """retrieve element list at index"""
     n = len(my_list)
-    if idx < 0 or idx > n:
+    if idx < 0 or idx >= n:
         return None
     else:
         return my_list[idx]
