a5abd34d2b5d9f0682061fefe06935916d648f39
aimad kacem <kacem.aimad@gmail.com>
2023-11-04 04:39:04 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list):
     """print all elements of my_list"""
     for i in range(len(my_list)):
-        print("{0:d}".format(my_list[i]))
+        print("{0}".format(my_list[i]))
