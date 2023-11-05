259d0c374d21085b6956fc7e106c7ba598d1cbfd
aimad kacem <kacem.aimad@gmail.com>
2023-11-05 04:19:07 -0800

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """remove c and C from string"""
+    if my_string:
+        chars = list(my_string)
+        i = 0
+        while i < len(chars):
+            if chars[i] == 'C' or chars[i] == 'c':
+                chars.pop(i)
+            else:
+                i += 1
+        new_string = "".join(chars)
+        return new_string
