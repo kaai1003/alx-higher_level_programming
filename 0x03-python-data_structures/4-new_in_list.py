4bea5ff2e3e95a0be15383807a32e713f83e0b98
aimad kacem <kacem.aimad@gmail.com>
2023-11-05 02:51:21 -0800

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        my_copy = my_list.copy()
+        if idx < 0 or idx >= len(my_list):
+            return my_copy
+        else:
+            my_copy[idx] = element
+            return my_copy
