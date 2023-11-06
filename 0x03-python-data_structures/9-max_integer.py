b07acdd85a2ca88edcd4de3ba98e40156148a81f
aimad kacem <kacem.aimad@gmail.com>
2023-11-06 01:45:09 -0800

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """return the biggest integer in list"""
+    if not my_list:
+        return None
+    else:
+        big_int = 0
+        for n in my_list:
+            if n > big_int:
+                big_int = n
+        return big_int
