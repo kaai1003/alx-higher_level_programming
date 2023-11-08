32800fd4bcecf3528486ec384ff865400d74a203
aimad kacem <kacem.aimad@gmail.com>
2023-11-08 15:46:04 -0800

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,5 +2,5 @@
 def simple_delete(a_dictionary, key=""):
     for k in a_dictionary.keys():
         if k == key:
-            a_dictionary.pop(key)
+            a_dictionary.pop(k)
     return a_dictionary
