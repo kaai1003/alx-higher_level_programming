92847b727f20ee1e7f7c9755085465b78dd908b4
aimad kacem <kacem.aimad@gmail.com>
2023-12-09 09:02:53 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, ssize=0):
-        try:
-            self.__size = ssize
-            if ssize < 0:
-                raise Exception("size must be >= 0")
-        except TypeError:
+        self.__size = ssize
+        if type(ssize) != int:
             raise Exception("size must be an integer")
+        elif ssize < 0:
+            raise Exception("size must be >= 0")
     def area(self):
         return self.__size**2
