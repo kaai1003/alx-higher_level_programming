d08ecdfddaeb4641e89b8134ed008db961a7b662
AIMAD KACEM <kacem.aimad@gmail.com>
2024-01-07 18:23:40 +0100

    6-max_integer_test.py

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""unittest module"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """UnitTest Class"""
+
+    def test_integer(self):
+        """test list of integers"""
+
+        list = [1, 3, 6, 3]
+        self.assertEqual(max_integer(list), 6)
+
+    def test_emptylist(self):
+        """test empty list"""
+
+        list = []
+        self.assertEqual(max_integer(list), None)
+
+    def test_float_list(self):
+        """test list of floats"""
+
+        list = [1.5, 0.5, 3.4, 1]
+        self.assertEqual(max_integer(list), 3.4)
+
+    def test_str_list(self):
+        """test list contains strings"""
+        
+        list = [3, "b", 5]
+        self.assertRaises(TypeError)
+        list = ["e", "s", "t", "a"]
+        self.assertEqual(max_integer(list), "t")
