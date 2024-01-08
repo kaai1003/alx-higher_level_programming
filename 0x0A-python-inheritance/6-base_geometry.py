4544976320084abd1f6e5f45fcb0d08090eb2e56
AIMAD KACEM <kacem.aimad@gmail.com>
2024-01-08 23:05:52 +0100

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""BaseGeometry Module"""
+
+
+class BaseGeometry:
+    """basegeometry class"""
+
+    def area(self):
+        """area Method
+        raise exception
+        """
+        raise Exception("area() is not implemented")
+
