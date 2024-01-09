29b47c405804aaf448f101aa8014a715b30af5aa
AIMAD KACEM <kacem.aimad@gmail.com>
2024-01-09 14:58:14 +0100

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,11 +9,8 @@ def print_square(size):
         size (int): size of square
     """
     if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     for r in range(size):
         print("#" * size)
-
-import doctest
-doctest.testfile("tests/4-print_square.txt", verbose=False)
