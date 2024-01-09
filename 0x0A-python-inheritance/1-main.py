e149148a87fc332848acab12ae1079a5681fa705
AIMAD KACEM <kacem.aimad@gmail.com>
2024-01-09 11:18:37 +0100

    testing !!!!

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -11,5 +11,11 @@ my_list.append(-2)
 print(my_list)
 my_list.print_sorted()
 print(my_list)
-new_list = my_list.print_sorted()
-print(new_list)
+try:
+    new_list = my_list.print_sorted()
+except Exception as e:
+    print(e)
+try:
+    print(new_list)
+except Exception as e:
+    print(e)
