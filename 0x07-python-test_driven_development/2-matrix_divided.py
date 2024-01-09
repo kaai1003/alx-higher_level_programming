2c06b003c7aae2f60f29d7bde87691312f166c4b
AIMAD KACEM <kacem.aimad@gmail.com>
2024-01-09 14:38:29 +0100

    debuging is so important

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,13 +3,14 @@
 
 
 def matrix_divided(matrix, div):
-    """a function that devid each member of matrix 
+    """a function that devid each member of matrix
+
         return the new matrix
     Args:
         matrix: matrix list of lists
         div: divisor integer of float
     """
-    if (div == 0 ):
+    if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
@@ -22,19 +23,23 @@ def matrix_divided(matrix, div):
         new_matrix.append(new_list)
     return (new_matrix)
 
+
 def check_matrix(matrix):
-    """function to check matrix 
+    """function to check matrix
+
         matrix should be list of list,
         element of lists should be integers/floats,
         size of lists should be the same
     Args:
-        matrix: 
+        matrix:
     """
     if not (all(isinstance(item, list) for item in matrix)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("""matrix must be a matrix
+                         (list of lists) of integers/floats""")
     for sublist in matrix:
         if not (all(isinstance(n, (int, float)) for n in sublist)):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("""matrix must be a matrix
+                             (list of lists) of integers/floats""")
     lists_length = set(len(list) for list in matrix)
     if len(lists_length) != 1:
         raise TypeError("Each row of the matrix must have the same size")
