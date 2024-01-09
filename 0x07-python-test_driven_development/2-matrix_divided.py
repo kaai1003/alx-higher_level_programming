ce0b84f3568ab15420e394d17238a30adbe8b705
AIMAD KACEM <kacem.aimad@gmail.com>
2024-01-09 14:55:02 +0100

    3-say_my_name.txt

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -34,12 +34,12 @@ def check_matrix(matrix):
         matrix:
     """
     if not (all(isinstance(item, list) for item in matrix)):
-        raise TypeError("""matrix must be a matrix
-                         (list of lists) of integers/floats""")
+        raise TypeError("""matrix must be a matrix\
+ (list of lists) of integers/floats""")
     for sublist in matrix:
         if not (all(isinstance(n, (int, float)) for n in sublist)):
-            raise TypeError("""matrix must be a matrix
-                             (list of lists) of integers/floats""")
+            raise TypeError("matrix must be a matrix\
+ (list of lists) of integers/floats")
     lists_length = set(len(list) for list in matrix)
     if len(lists_length) != 1:
         raise TypeError("Each row of the matrix must have the same size")
