9199bba07a765f84fcd3f41d3d24ce54b08f50ee
AIMAD KACEM <kacem.aimad@gmail.com>
2024-01-10 17:35:11 +0100

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """JSON repr Module"""
 
+
 import json
 
+
 def from_json_string(my_str):
-    """return json representation"""
+    """return json representation
+
+    Args:
+        my_str (str): string variable
+    """
 
     return json.dumps(my_str)
