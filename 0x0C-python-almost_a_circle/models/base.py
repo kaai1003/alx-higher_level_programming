198762b3edc8856e078490b1b33a928076a30ab1
AIMAD KACEM <kacem.aimad@gmail.com>
2024-01-16 15:01:07 +0100

    modify create class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -73,7 +73,10 @@ class Base:
         Args:
             dictionary (dict): dictionary of attributes
         """
-        dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
