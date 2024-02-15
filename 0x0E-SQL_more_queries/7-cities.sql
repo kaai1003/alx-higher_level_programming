824a68ee25614ca6b9fe39bacad982bafd584660
AIMAD KACEM <kacem.aimad@gmail.com>
2024-02-15 23:38:36 +0100

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     PRIMARY KEY(id),
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES state(id),
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
