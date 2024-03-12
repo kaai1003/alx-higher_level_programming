56a134984c7b34b86ba158588e7a6931e64252b7
aimad kacem <kacem.aimad@gmail.com>
2024-03-12 11:10:28 +0000

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function factorial (num) {
+  if (isNaN(num) || num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
