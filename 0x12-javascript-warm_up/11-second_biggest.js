a5a5185df45fd7a4a72d91e9ea05c416e0613b56
aimad kacem <kacem.aimad@gmail.com>
2024-03-12 11:34:59 +0000

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  let b = process.argv[2];
+  let sb = -Infinity;
+  for (let i = 3; i < process.argv.length; i++) {
+    if (process.argv[i] > b) {
+      sb = b;
+      b = process.argv[i];
+    } else if (process.argv[i] < b && process.argv[i] > sb) {
+      sb = process.argv[i];
+    }
+  }
+  console.log(sb);
+}
