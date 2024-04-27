d4c834e094e044f8b7a98e9557b56066256ca7ce
AIMAD KACEM <kacem.aimad@gmail.com>
2024-04-27 01:25:05 +0100

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""send http request using Request
+display variable X-Request-Id
+Usage: script_name URL"""
+import urllib
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
