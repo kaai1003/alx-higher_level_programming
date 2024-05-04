8cf35b4890dd2bf938d6270a8fc29e5913ca8b40
AIMAD KACEM <kacem.aimad@gmail.com>
2024-05-04 01:32:11 +0100

    0-body_size.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #display all methods accpeted by server URL
-curl -i "$1" | grep 'Allow:' | cut -d ":" -f 2 | cut -c 2-
+curl -i --no-progress-meter "$1" | grep 'Allow:' | cut -d ":" -f 2 | cut -c 2-
