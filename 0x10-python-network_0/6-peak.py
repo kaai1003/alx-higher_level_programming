dfd78a6f383052872973a7750cfac62414d1d3dc
AIMAD KACEM <kacem.aimad@gmail.com>
2024-05-04 02:01:50 +0100

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,11 +14,11 @@ def find_peak(list_of_integers):
     end = len(list_of_integers) - 1
 
     if list_of_integers[start] > list_of_integers[start + 1] and\
-        start < end:
+            start < end:
         return list_of_integers[start]
-    elif list_of_integers[end] > list_of_integers[end -1] and\
-        end > start:
-        return list_of_integers[end] 
+    elif list_of_integers[end] > list_of_integers[end - 1] and\
+            end > start:
+        return list_of_integers[end]
     while start <= end:
         mid = (start + end) // 2
         if (mid == 0 or
