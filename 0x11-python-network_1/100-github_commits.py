5efa888884153a6991853bb185cb30cd963035c1
AIMAD KACEM <kacem.aimad@gmail.com>
2024-05-05 01:13:49 +0100

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""list 10 commit of given repo"""
+import requests
+import sys
+
+
+url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
+                                                          sys.argv[2])
+try:
+    r = requests.get(url)
+    r_dict = r.json()
+    i = 0
+    for item in r_dict:
+        if i < 10:
+            print("{}: {}".format(item['sha'],
+                                  item['commit']['author']['name']))
+        else:
+            break
+        i += 1
+except Exception as e:
+    print(e)
